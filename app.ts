c7215ae5106a15ac95f9476b8db72364e48e0a44
Augustya Sharma <augustya.s@tmax.in>
2022-11-01 09:59:01 +0530

    push

--- app.ts
+++ app.ts
@@ -31,6 +31,7 @@ import {Request, Response} from "express";
 const app = express();
 // const port = 5000;
 
+
 //serve client-pms/build from express
 // app.get('/', (req, res) => {
 //     res.sendFile(path.join(__dirname, "../", 'build', 'index.html'));
