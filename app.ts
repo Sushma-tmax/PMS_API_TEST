7780072177144cb9939e2fadd5d1aa63c7a90ec5
meenakshi-rvkmr <meenakshi.rvkmr@gmail.com>
2023-07-05 08:17:00 +0400

    test update

--- app.ts
+++ app.ts
@@ -60,7 +60,7 @@ dotenv.config({path: __dirname + '/.env'});
 // connectDB('mongodb://127.0.0.1:27017/pms');
 
  connectDB('mongodb+srv://augustya:brOlGGq5fjj5EL5z@cluster0.22mx4.mongodb.net/PMS?retryWrites=true&w=majority');
-
+//connectDB('mongodb+srv://mongodb04:7CAV3kA65Td9xn9m@pmstest2cluster.cfpmcjd.mongodb.net/PMS?retryWrites=true&w=majority');
 
 // connectDB('mongodb+srv://pms:yGMXa8yrjjnjydFc@cluster0.axzjq.mongodb.net/PMS?retryWrites=true&w=majority');
 console.log(process.env.MONGO_URI);
