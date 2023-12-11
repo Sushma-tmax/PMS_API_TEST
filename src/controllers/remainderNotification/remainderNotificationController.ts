9bcd9fbd27e9c7d4ad02596f4cfb8a4732e223b5
Meenakshi S <meenakshi.rvkmr@gmail.com>
2023-12-11 10:24:59 +0400

    Update remainderNotificationController.ts

--- src/controllers/remainderNotification/remainderNotificationController.ts
+++ src/controllers/remainderNotification/remainderNotificationController.ts
@@ -288,15 +288,31 @@ async function dailyTask() {
 //   const timeUntilNextRun = nextMinute.getTime() - now.getTime();
 //   return timeUntilNextRun;
 // }
-// Function to calculate the time until the next run (every 2 min).
+// Function to calculate the time until the next run (every day at 8 am).
 function calculateTimeUntilNextRun() {
   const now = new Date();
-  const nextMinute = new Date(now);
-  nextMinute.setMinutes(now.getMinutes() + 2);
-  nextMinute.setSeconds(0);
-  const timeUntilNextRun = nextMinute.getTime() - now.getTime();
+  let nextRun = new Date(now);
+ 
+  // Set the next run time to 8 am.
+  nextRun.setHours(8, 0, 0, 0);
+ 
+  // If the current time is already past 8 am, set the next run to the next day.
+  if (now >= nextRun) {
+    nextRun.setDate(now.getDate() + 1);
+  }
+ 
+  const timeUntilNextRun = nextRun.getTime() - now.getTime();
   return timeUntilNextRun;
 }
+// Function to calculate the time until the next run (every 2 min).
+// function calculateTimeUntilNextRun() {
+//   const now = new Date();
+//   const nextMinute = new Date(now);
+//   nextMinute.setMinutes(now.getMinutes() + 2);
+//   nextMinute.setSeconds(0);
+//   const timeUntilNextRun = nextMinute.getTime() - now.getTime();
+//   return timeUntilNextRun;
+// }
 // Function to calculate the time until the next run (every 24 hours).
 // function calculateTimeUntilNextRun() {
 //   const now = new Date();
